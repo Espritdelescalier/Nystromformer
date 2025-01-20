e9ae89dfc64209a403d8b2e59ff3ac97f4511407
Mathis Saillot <mathis.saillot@gmail.com>
2025-01-21 00:25:32 +0100

    run tasks

--- LRA/code/run_tasks.sh
+++ LRA/code/run_tasks.sh
@@ -4,8 +4,9 @@ task_list=("listops" "text" "retrieval" "image" "pathfinder32-curv_contour_lengt
 model=$2
 opt=$3
 
-for _ in {1..5} ; do
-  for task in "${task_list[@]}" ; do
+
+for task in "${task_list[@]}" ; do
+  for _ in {1..5} ; do
     CUDA_VISIBLE_DEVICES=$1 python run_tasks.py --model $model --task $task $opt
   done
 done
